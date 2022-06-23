8b429f6f8db5e55ae051759b880e2a275bb0d703
georgiamoore-softwire <107865029+georgiamoore-softwire@users.noreply.github.com>
2022-06-23 14:36:26 +0100

    Finished part 2

--- index.js
+++ index.js
@@ -1,42 +1,51 @@
 import axios from 'axios';
 import {question} from 'readline-sync';
+import log4js from 'log4js';
 
-//https://api.tfl.gov.uk/StopPoint/{id}/Arrivals
+log4js.configure({
+    appenders: {
+        file: { type: 'fileSync', filename: 'logs/debug.log' }
+    },
+    categories: {
+        default: { appenders: ['file'], level: 'warn'}
+    }
+});
 
-getNearestBusStops('W1A 1AA')
-// NW51TL
-//  W1A 1AA
-// const stopCode = question('Please type a bus stopcode.\n').trim();
+const logger = log4js.getLogger('index.js');
 
-// getDepartureInfo(stopCode)
-// let stopCode = '490012409N'
+logger.info("Asking user for postcode");
+const postcode = question('Please type a postcode.\n').trim();
+getNearestBusStops(postcode) //NW51TL
+
 
 async function getDepartureInfo (stopCode)  {
+    logger.info("Getting departure information from stopCode: " + stopCode);
     axios.get('https://api.tfl.gov.uk/StopPoint/' + stopCode + '/Arrivals')
-        .then((response) =>{
-            // handle success
-            // console.log(response.data);
+        .then((response) => {
             let busInfo = response.data;
-            // TODO: sort bus by timeToStation
-            busInfo = busInfo.sort((a,b) => a.timeToStation > b.timeToStation ? 1 : -1);
-            for (let i = 0; i < Math.min(5, busInfo.length); i++) {
+            console.log("Station name: " + busInfo[0].stationName +'\n');
+            if (busInfo.length > 0) { // buses are arriving
+                busInfo = busInfo.sort((a, b) => a.timeToStation > b.timeToStation ? 1 : -1);
+                for (let i = 0; i < Math.min(5, busInfo.length); i++) {
 
-                console.log("Line Name: " + busInfo[i].lineName);
-                console.log("Destination Name: " + busInfo[i].destinationName);
-                console.log("Towards: " + busInfo[i].towards);
-                console.log("Time to station: " + Math.round(busInfo[i].timeToStation / 60) + " minutes");
-                console.log();
+                    console.log("Line Name: " + busInfo[i].lineName);
+                    console.log("Destination Name: " + busInfo[i].destinationName);
+                    console.log("Towards: " + busInfo[i].towards);
+                    console.log("Time to station: " + Math.round(busInfo[i].timeToStation / 60) + " minutes");
+                    console.log();
+                }
             }
-
         })
         .catch((error) => {
-            console.log(error);
+            console.log("Error in finding bus stop: " + error.response.data.message);
         })
 }
 
 async function getPostcodeLocation (postcode) {
+    logger.info("Retrieving postcode longitude and latitude");
     let response = await axios.get('http://api.postcodes.io/postcodes/' + postcode)
                                 .catch((error) => {
+                                    logger.warn("Error retrieving postcode: " + error);
                                     console.log(error);
                                 })
     //todo determine if api response is valid
@@ -46,26 +55,14 @@ async function getPostcodeLocation (postcode) {
 async function getNearestBusStops (postcode) {
     let [lat,lon] = await getPostcodeLocation(postcode);
     const stopTypes = "NaptanPublicBusCoachTram";
-    //todo connect this to tfl
+
     let nearestBusStops = (await axios.get('https://api.tfl.gov.uk/StopPoint/?lat=' + lat + '&lon=' + lon +
         '&stopTypes=' + stopTypes + '&radius=' + 500)).data.stopPoints;
-    // nearestBusStops = nearestBusStops.sort((a,b) => a.distance > b.distance ? 1 : -1);  -
+
     for (let i = 0; i < Math.min(2, nearestBusStops.length); i++) {
-        console.log("Station name: " + nearestBusStops[i].stationName);
-        console.log(await getDepartureInfo(nearestBusStops[i].id));
+        await getDepartureInfo(nearestBusStops[i].id);
+
     }
-    //console.log(nearestBusStops);
 
 }
-///?lat={lat}&lon={lon}&stopTypes={stopTypes}
 
-/* ["CarPickupSetDownArea","NaptanAirAccessArea","NaptanAirEntrance","NaptanAirportBuilding","NaptanBusCoachStation",
-"NaptanBusWayPoint","NaptanCoachAccessArea","NaptanCoachBay","NaptanCoachEntrance","NaptanCoachServiceCoverage",
-"NaptanCoachVariableBay","NaptanFerryAccessArea","NaptanFerryBerth","NaptanFerryEntrance","NaptanFerryPort",
-"NaptanFlexibleZone","NaptanHailAndRideSection","NaptanLiftCableCarAccessArea","NaptanLiftCableCarEntrance",
-"NaptanLiftCableCarStop","NaptanLiftCableCarStopArea","NaptanMarkedPoint","NaptanMetroAccessArea","NaptanMetroEntrance",
-"NaptanMetroPlatform","NaptanMetroStation","NaptanOnstreetBusCoachStopCluster","NaptanOnstreetBusCoachStopPair",
-"NaptanPrivateBusCoachTram","NaptanPublicBusCoachTram","NaptanRailAccessArea","NaptanRailEntrance","NaptanRailPlatform",
-"NaptanRailStation","NaptanSharedTaxi","NaptanTaxiRank","NaptanUnmarkedPoint","TransportInterchange"]
-
- */
